293af55cf87d9201669bf6413b60e14c30bf8b69
SanVeasna <sanveasna096@gmail.com>
2023-03-21 00:20:15 +0700

    update

---
+++ layout/NavLeft.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ul>
+            <li><NuxtLink to="/home">Home</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+
+</style>
