1036613d47a7fc88a89df865328c36c8008ab6e8
SanVeasna <sanveasna096@gmail.com>
2023-04-05 09:55:44 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
   <h1>Index</h1>
+  <template v-for="dd in products">
+      <p>
+          <div>{{ dd.title }}</div>
+          <NuxtLink :to="`/products/${dd.id}`">Detail...</NuxtLink>
+      </p>
+  </template>
 </template>
 <script setup>
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
 </script>
---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>
+            <h1>{{ data.title }}</h1>
+            <p>{{ data.description }}</p>
+        </p>
+    </div>
+</template>
+<script lang="ts" setup>
+interface product{
+    title:string,
+    description:string
+}
+const { id } = useRoute().params;
+const url = "https://fakestoreapi.com/products/" + id
+const { data} = await useFetch(url);
+</script>
